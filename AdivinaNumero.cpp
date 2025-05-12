d3a66ff7367e84c39c1421ac04114afcecc67b26
VLADIMIR CUEVAS <66849394+FvladimirC@users.noreply.github.com>
2025-05-11 20:52:13 -0400

    AdivinaNumero.cpp

--- AdivinaNumero.cpp
+++ AdivinaNumero.cpp
@@ -9,14 +9,13 @@ int main() {
     int x;
     int y = 0;
     int numero;
-    // Inicializa el generador de números aleatorios con la hora actual
+    
     srand(time(NULL));
-    // Genera un número aleatorio entre 1 y 100 (inclusive)
-    numero = rand() % 100 + 1;
+    // Genera un número aleatorio entre 1 y 100
 
     cout << "¡Adivina el número secreto entre 1 y 100!\n";
 
-    // Bucle while que continúa hasta que el usuario adivina el número
+    // Bucle while que continua hasta que el usuario adivina el número
     while (true) {
         cout << "Introduce tu intento: ";
         cin >> x;
